7d7f70145ebe6934176fb9a81056cf6f3b961213
LittleCadet <sx255879@163.com>
2018-06-21 15:14:13 +0800

    需求编号：NAN 修改描述：编程大赛 修改人：沈燮 修改日期：2018-06-21

--- src/main/java/com/migu/schedule/Schedule.java
+++ src/main/java/com/migu/schedule/Schedule.java
@@ -1,55 +1,85 @@
 package com.migu.schedule;
 
-
 import com.migu.schedule.constants.ReturnCodeKeys;
 import com.migu.schedule.info.TaskInfo;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /*
-*类名和方法不能修改
+ *类名和方法不能修改
  */
 public class Schedule {
 
+	private List<Integer> nodeList;
+	
+	private Map<Integer, Integer> map;
+	
 
-    public int init() {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
-    }
+	public int init() {
 
+		nodeList = new ArrayList<Integer>();
+		map = new HashMap<Integer, Integer>();
+		return ReturnCodeKeys.E001;
+	}
 
-    public int registerNode(int nodeId) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
-    }
+	public int registerNode(int nodeId) {
+		if (0 >= nodeId) {
+			return ReturnCodeKeys.E004;
+		}
+		if (nodeList.contains(nodeId)) {
+			return ReturnCodeKeys.E005;
+		}
 
-    public int unregisterNode(int nodeId) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
-    }
+		nodeList.add(nodeId);
+		return ReturnCodeKeys.E003;
+	}
 
+	public int unregisterNode(int nodeId) {
+		if (null != map && map.containsKey(nodeId))
+		{
+			int taskId = map.get(nodeId);			
+			map.remove(nodeId);
+			
+			if (map.containsKey(taskId))
+			{
+				int consumption = map.get(taskId);
+				map.put(taskId, consumption);
+			}
+		}
+		if (0 >= nodeId) {
+			return ReturnCodeKeys.E004;
+		}
+		if (!nodeList.contains(nodeId)) {
+			return ReturnCodeKeys.E007;
+		}
+		return ReturnCodeKeys.E006;
+	}
 
-    public int addTask(int taskId, int consumption) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
-    }
+	public int addTask(int taskId, int consumption) {
+		return ReturnCodeKeys.E000;
+	}
 
+	public int deleteTask(int taskId) {
+		
+		return ReturnCodeKeys.E000;
+	}
 
-    public int deleteTask(int taskId) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
-    }
+	public int scheduleTask(int threshold) {
+		// 调度阈值非法
+		if (0 > threshold) {
+			return ReturnCodeKeys.E002;
+		}
 
+		// TODO 方法未实现
+		return ReturnCodeKeys.E000;
+	}
 
-    public int scheduleTask(int threshold) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
-    }
-
-
-    public int queryTaskStatus(List<TaskInfo> tasks) {
-        // TODO 方法未实现
-        return ReturnCodeKeys.E000;
-    }
+	public int queryTaskStatus(List<TaskInfo> tasks) {
+		// TODO 方法未实现
+		return ReturnCodeKeys.E000;
+	}
 
 }
